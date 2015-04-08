1152f2ceb1815de9f2c783c57c9ad45b9934145d
Matias Keveri <matias@keveri.fi>
2015-04-08 16:19:38 +0200

    Remove unused files. Move global rest configs to own class.

---
+++ UserManagementApp/UserManagementApp-ejb/src/main/java/ua/group06/rest/RestResource.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.group06.rest;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author matson
+ */
+@Stateless
+@ApplicationPath("/resources")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class RestResource extends Application {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
--- UserManagementApp/UserManagementApp-ejb/src/main/java/ua/group06/rest/UserService.java
+++ UserManagementApp/UserManagementApp-ejb/src/main/java/ua/group06/rest/UserService.java
@@ -7,13 +7,8 @@ package ua.group06.rest;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import ua.group06.logic.UserRegistrationServiceLocal;
 import ua.group06.persistence.User;
@@ -23,11 +18,8 @@ import ua.group06.persistence.User;
  * @author matson
  */
 @Stateless
-@ApplicationPath("/resources")
 @Path("users")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
-public class UserService extends Application {
+public class UserService extends RestResource {
     @EJB
     private UserRegistrationServiceLocal regService;
     
