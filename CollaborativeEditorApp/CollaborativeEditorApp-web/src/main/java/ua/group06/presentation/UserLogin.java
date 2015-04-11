b316fa00c0a2b834b88a5eb36b39d1f3482cdfaf
Matias Keveri <matias@keveri.fi>
2015-04-11 11:36:16 +0200

    Remove unused imports.

--- CollaborativeEditorApp/CollaborativeEditorApp-web/src/main/java/ua/group06/presentation/UserLogin.java
+++ CollaborativeEditorApp/CollaborativeEditorApp-web/src/main/java/ua/group06/presentation/UserLogin.java
@@ -6,7 +6,6 @@
 package ua.group06.presentation;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
