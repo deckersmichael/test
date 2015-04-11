918b090ba80913b341f4238be3a4f8297a03bed3
Matias Keveri <matias@keveri.fi>
2015-04-11 11:50:04 +0200

    Add user to session in login.

--- CollaborativeEditorApp/CollaborativeEditorApp-web/src/main/java/ua/group06/presentation/UserLogin.java
+++ CollaborativeEditorApp/CollaborativeEditorApp-web/src/main/java/ua/group06/presentation/UserLogin.java
@@ -12,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import ua.group06.entities.User;
 import ua.group06.logic.UserServiceLocal;
 
@@ -53,10 +54,11 @@ public class UserLogin extends HttpServlet {
         String email    = request.getParameter("email");
         String password = request.getParameter("password");
         User user = userService.login(email, password);
-        String loggedIn = (user != null ? "Yes" : "No"); // For debugging
-        request.setAttribute("loggedIn", loggedIn);
-        request.setAttribute("user", user);
-        request.getRequestDispatcher("homepage.jsp").forward(request, response);
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+        }
+        response.sendRedirect("homepage");
     }
 
     /**
