bb6742632dbe7d997418947e5375b330db1f99e3
Matias Keveri <matias@keveri.fi>
2015-04-14 11:15:45 +0200

    Create Session model for storing our session related stuff.

---
+++ CollaborativeEditorApp/CollaborativeEditorApp-ejb/src/main/java/ua/group06/persistence/Session.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.group06.persistence;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author matson
+ */
+@Entity
+@Table(name="SESSIONS")
+public class Session implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String token;
+    private Long userId;
+    
+    public Session() {}
+    
+    public Session(Long uid, String token) {
+        this.userId = uid;
+        this.token  = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Session)) {
+            return false;
+        }
+        Session other = (Session) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ua.group06.persistence.Session[ id=" + id + " ]";
+    }
+    
+}
