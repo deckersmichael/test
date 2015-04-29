10adb294de7d0ae29ac564a4f5008190bb490074
Yves Maris <Yves Maris@192.168.0.22>
2015-04-29 21:52:54 +0200

    Added logic for modifying users

---
+++ UserManagementApp/UserManagementApp-ejb/src/main/java/ua/group06/logic/UserSettingsService.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.group06.logic;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.persistence.EntityExistsException;
+import ua.group06.persistence.User;
+import ua.group06.business.UserFacadeLocal;
+
+/**
+ *
+ * @author Yves Maris
+ */
+@Stateless
+public class UserSettingsService implements UserSettingsServiceLocal {
+    @EJB
+    private UserFacadeLocal userFacade;
+    @EJB
+    private PasswordEncryptionServiceLocal passwordService;
+    @Override
+    public User edit(User user) {
+        try {
+            userFacade.create( passwordService.encrypt(user) );
+        } catch (EntityExistsException e) {
+            // TODO: Should we change the return type or should we throw exception?
+            user = null;
+        }
+        return user;
+    }
+    
+}
---
+++ UserManagementApp/UserManagementApp-ejb/src/main/java/ua/group06/logic/UserSettingsServiceLocal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.group06.logic;
+
+import javax.ejb.Local;
+import ua.group06.persistence.User;
+
+/**
+ *
+ * @author Yves Maris
+ */
+@Local
+public interface UserSettingsServiceLocal {
+    User edit(User user);
+}
